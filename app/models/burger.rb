569245d170f4e3fe24d67cc602e5b4c8aa888eec
Antoine Muré <antoine.mure.am@gmail.com>
2020-03-11 11:17:54 +0100

    add @ before variable name

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,7 +1,7 @@
 class Burger < ApplicationRecord
     def nutriments(code)
-        product = Openfoodfacts::Product.get(code, locale: 'fr')
-        json = product.nutriments.to_hash
-        return json
+        @product = Openfoodfacts::Product.get(code, locale: 'fr')
+        @json = product.nutriments.to_hash
+        return @json
     end
 end

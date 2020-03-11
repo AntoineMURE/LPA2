ddaab53926c304799699a682cab7377379d63412
Antoine Muré <antoine.mure.am@gmail.com>
2020-03-11 11:11:23 +0100

    add nutriments method in model

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,7 @@
 class Burger < ApplicationRecord
+    def nutriments(code)
+        product = Openfoodfacts::Product.get(code, locale: 'fr')
+        json = product.nutriments.to_hash
+        return json
+    end
 end
